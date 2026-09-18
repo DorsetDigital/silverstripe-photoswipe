1ea8d52890f8d9eb3167357f0fc61686b1f2a389
Tim <DorsetDigital@users.noreply.github.com>
2026-09-18 20:30:06 +0200

    Improve gallery CSS and split PhotoSwipe styles

--- client/src/gallery.js
+++ client/src/gallery.js
@@ -1,6 +1,4 @@
 import PhotoSwipeLightbox from 'photoswipe/lightbox';
-import 'photoswipe/style.css';
-import './gallery.css';
 
 document.querySelectorAll('[data-pswp-gallery]').forEach((gallery) => {
   const lightbox = new PhotoSwipeLightbox({
