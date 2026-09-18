04ef826352484a085e8664a611611477c39dc5d1
Tim <DorsetDigital@users.noreply.github.com>
2026-09-18 19:57:55 +0200

    Connect gallery page controller

--- src/Pages/GalleryPage.php
+++ src/Pages/GalleryPage.php
@@ -2,15 +2,14 @@
 
 namespace DorsetDigital\SilverstripePhotoswipe\Pages;
 
+use DorsetDigital\SilverstripePhotoswipe\Controllers\GalleryPageController;
 use Page;
 
 class GalleryPage extends Page
 {
     private static string $table_name = 'PhotoSwipeGalleryPage';
-
     private static string $singular_name = 'Gallery Page';
-
     private static string $plural_name = 'Gallery Pages';
-
     private static string $description = 'A page containing a responsive image gallery';
+    private static string $controller_name = GalleryPageController::class;
 }
